5afaf38ce38ed52fcddf3d24508ce7d1c7cfe849
Leo Soares <106386261+StarLord2401@users.noreply.github.com>
2024-04-14 13:24:27 +0000

    git commit -m "v2"

--- src/control/ProdutoController.java
+++ src/control/ProdutoController.java
@@ -4,10 +4,10 @@ import model.Marca;
 import model.Produto;
 
 public class ProdutoController {
-    
-    public boolean add(Produto produto, Produto[] vetor){
-        if (produto != null && vetor != null) 
-            for (int i = 0; i < vetor.length; i++) 
+
+    public boolean add(Produto produto, Produto[] vetor) {
+        if (produto != null && vetor != null)
+            for (int i = 0; i < vetor.length; i++)
                 if (vetor[i] == null) {
                     vetor[i] = produto;
                     return true;
@@ -15,22 +15,22 @@ public class ProdutoController {
         return false;
     }
 
-    public boolean delete(int idProduto, Produto[] vetor){
+    public boolean delete(int idProduto, Produto[] vetor) {
         int position = -1;
-        for (int i = 0; i < vetor.length; i++) 
+        for (int i = 0; i < vetor.length; i++)
             if (vetor[i] != null && vetor[i].getIdProduto() == idProduto) {
                 vetor[i] = null;
                 position = i;
             }
         if (position != -1) {
-            for (int i = position; i < vetor.length-1; i++)
-                vetor[position] = vetor[i+1];
+            for (int i = position; i < vetor.length - 1; i++)
+                vetor[position] = vetor[i + 1];
             return true;
         }
         return false;
     }
 
-    public Produto findById(int idProduto, Produto[] vetor){
+    public Produto findById(int idProduto, Produto[] vetor) {
         Produto p;
         for (int i = 0; i < vetor.length; i++)
             if (vetor[i] != null && vetor[i].getIdProduto() == idProduto) {
@@ -40,14 +40,16 @@ public class ProdutoController {
         return p = null;
     }
 
-    public boolean update(int idProduto, Produto novo, Produto[] vetor){
-        if(novo != null){
-            for (int i = 0; i < vetor.length; i++) 
+    public boolean update(int idProduto, Produto novo, Produto[] vetor) {
+        if (novo != null) {
+            for (int i = 0; i < vetor.length; i++)
                 if (vetor[i] != null && vetor[i].getIdProduto() == idProduto) {
                     vetor[i].setNome(novo.getNome());
                     vetor[i].setPreco(novo.getPreco());
-                    vetor[i].setMarca(Marca.getInstance(novo.getMarca()));;
-                    vetor[i].setEstoque(novo.getEstoque());;                    
+                    vetor[i].setMarca(Marca.getInstance(novo.getMarca()));
+                    ;
+                    vetor[i].setEstoque(novo.getEstoque());
+                    ;
                     return true;
                 }
         }
--- src/view/ProdutoView.java
+++ src/view/ProdutoView.java
@@ -8,8 +8,7 @@ import model.*;
 public class ProdutoView {
     private static Scanner scanner = new Scanner(System.in);
     private static MarcaController mController = new MarcaController();
-    private ProdutoController pController;
-    
+
     public Produto lerProduto() {
         Produto produto = Produto.getInstance();
         System.out.println("---------- PRODUTO -----------");
@@ -22,31 +21,112 @@ public class ProdutoView {
         Marca marca = mController.verificarMarca(texto);
         if (marca != null)
             produto.setMarca(marca);
-        else{
+        else {
             marca = Marca.getInstance(texto);
             mController.add(marca);
             produto.setMarca(marca);
         }
         System.out.print("Quantidade em estoque: ");
+        scanner.nextLine();
         produto.setEstoque(scanner.nextInt());
 
         return produto;
     }
 
-    public void show(Produto produto){
-        System.out.println("---------- PRODUTO -----------");
-        System.out.println("Código do produto: " + produto.getIdProduto());
-        System.out.println("Nome do produto: " + produto.getNome());
-        System.out.println("Preço do produto: " + produto.getPreco());
-        System.out.println("Marca do produto: " + produto.getMarca());
-        System.out.println("Quantidade em estoque: " + produto.getEstoque());
+    public void show(Produto produto) {
+        System.out.println("\n");
+        System.out.println("- - - - - - - - PRODUTO - - - - - - - -");
+        System.out.print("Código do produto:    ");
+        System.out.printf("%17s", produto.getIdProduto());
+        System.out.println();
+        System.out.print("Nome do produto:      ");
+        System.out.printf("%17s", produto.getNome());
+        System.out.println();
+        System.out.print("Preço do produto:     ");
+        System.out.printf("%17s", produto.getPreco());
+        System.out.println();
+        System.out.print("Marca do produto:     ");
+        System.out.printf("%17s", produto.getMarca());
+        System.out.println();
+        System.out.print("Quantidade em estoque:");
+        System.out.printf("%17s", produto.getEstoque());
+        System.out.println();
     }
 
-    public void showAll(Produto[] vetor){
-        for (int i = 0; i < vetor.length; i++) 
+    public void showInList(Produto produto) {
+        System.out.printf("%-17s", produto.getIdProduto());
+        System.out.printf("%-17s", produto.getNome());
+        System.out.printf("%-17s", produto.getPreco());
+        System.out.printf("%-17s", produto.getMarca());
+        System.out.printf("%-17s", produto.getEstoque());
+    }
+
+    // Listagem em ORDEM DE CADASTRO
+    public void showAll(Produto[] vetor) {
+        header();
+        for (int i = 0; i < vetor.length; i++)
             if (vetor[i] != null) {
                 System.out.println();
-                show(vetor[i]);
+                showInList(vetor[i]);
             }
+    }
+
+    // Listagem em ORDEM ALFABÉTICA
+    public void showAllInOrder(Produto[] produtos) {
+        String[] vetAux = new String[cont(produtos)];
+
+        for (int i = 0; i < produtos.length; i++)
+            if (produtos[i] != null)
+                vetAux[i] = produtos[i].getNome();
+
+        vetAux = order(vetAux);
+
+        header();
+
+        for (int i = 0; i < vetAux.length; i++) {
+            System.out.println();
+            listByName(vetAux[i], produtos);
+        }
+    }
+
+    // CONTA O NÚMERO DE PRODUTOS REGISTRADOS
+    private int cont(Produto[] produtos) {
+        int contador = 0;
+
+        for (int i = 0; i < produtos.length; i++)
+            if (produtos[i] != null)
+                contador++;
+
+        return contador;
+    }
+
+    private void listByName(String string, Produto[] produtos) {
+        for (int i = 0; i < produtos.length; i++)
+            if (produtos[i] != null && produtos[i].getNome() == string)
+                showInList(produtos[i]);
+    }
+
+    private String[] order(String[] vetAux) {
+        String aux;
+
+        for (int i = 0; i < vetAux.length; i++)
+            for (int j = 0; j < vetAux.length; j++)
+                if (vetAux[j].compareTo(vetAux[i]) > 0) {
+                    aux = vetAux[i];
+                    vetAux[i] = vetAux[j];
+                    vetAux[j] = aux;
+                }
+
+        return vetAux;
+    }
+
+    // Bloco de TEXTO
+    private void header() {
+        System.out.printf("%-17s", "CÓDIGO");
+        System.out.printf("%-17s", "NOME");
+        System.out.printf("%-17s", "PREÇO");
+        System.out.printf("%-17s", "MARCA");
+        System.out.printf("%-17s", "ESTOQUE");
+        System.out.println();
     }
 }
